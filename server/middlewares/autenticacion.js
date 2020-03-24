a02a8ea73a4f7dc1a57e0bd0ecf3bbf62d59da99
McPepe26 <andyzam301@gmail.com>
2020-03-24 15:31:16 -0600

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -19,7 +19,7 @@ let verificaToken = (req, res, next) =>{
         req.usuario = decoded.usuario;
         next();
     });
-};
+}
 
 // ================================
 // Verificar Admin Role
@@ -36,9 +36,32 @@ let verificaAdminRole = (req, res, next) => {
         });
     }
     next();
-};
+}
+
+// ================================
+// Verificar token para imagen
+// ================================
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if(err){
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no valido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+}
+
 
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
